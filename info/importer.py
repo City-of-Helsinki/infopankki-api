21cc417b331174b30fc77d829387ec9678deb9a1
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-09-26 11:49:43 +0300

    WIP Transforming page data to Python in importer

--- info/importer.py
+++ info/importer.py
@@ -1,18 +1,48 @@
 
 from lxml import etree
 from info.models import MasterPage, Page
+from collections import namedtuple, defaultdict
+
+PageData = namedtuple('PageData', ['languages', 'documents', 'meta', 'guid'])
 
 
 def read(doc='data/sivurakenne.xml'):
-
     x = etree.parse(doc)
+    return [transform(page )for page in x.xpath('//page')]
 
-    pages = x.xpath('//page')
 
-    for master_page in pages:
-        MP = MasterPage(page_guid=master_page.attrib['guid'])
-        MP.save()
+def parse_meta(items):
+    """
+    Creates dictionary from meta data where each key and its value
+    is put under its language
 
-        for page in master_page.xpath('activelanguages/language'):
-            P = Page(master=MP, language=page.text)
-            P.save()
+    :param items:[item element]
+    :return:{{}}
+    """
+    meta = defaultdict(dict)
+
+    for item in items:
+        data = item.attrib
+        lang = data.get('language', False)
+        if lang:
+            meta[lang][data['name']] = item.text
+        else:
+            meta[data['name']] = item.text
+
+    return meta
+
+
+def transform(page):
+    """
+    Transform page elements into PageData object
+    :param page:page element
+    :return:PageData
+    """
+
+    return PageData(
+        guid=page.attrib['guid'],
+        languages=[elem.text for elem in page.xpath('activelanguages/language')],
+        documents=[{'id': doc.attrib['id'], 'title': doc.attrib['title']}
+                   for doc in page.xpath('controls/control/configuration/document')],
+        meta=parse_meta(page.xpath('meta/item'))
+    )
