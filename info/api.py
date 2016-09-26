b4de5653a41f3f685078633d0bdf84dfb5e78a61
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-09-26 20:16:15 +0300

    Show content on detail view

--- info/api.py
+++ info/api.py
@@ -3,11 +3,28 @@ from info.models import MasterPage, Page
 from rest_framework import serializers, viewsets
 
 
-class PageSerializer(serializers.ModelSerializer):
+class PageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Page
+        fields = ['language', 'meta', 'created', 'modified', 'url']
+        read_only_fields = ['language', 'meta', 'created', 'modified', 'url']
+
+
+class PageDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Page
+        fields = ['language', 'meta', 'created', 'modified', 'content']
+        read_only_fields = ['language', 'meta', 'created', 'modified', 'url']
 
 
 class PageViewSet(viewsets.ModelViewSet):
+
     queryset = Page.objects.all().order_by('modified')
-    serializer_class = PageSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PageSerializer
+        elif self.action == 'retrieve':
+            return PageDetailSerializer
+        else:
+            return PageSerializer
