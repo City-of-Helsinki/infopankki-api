74cbf8acfdd290f94a75b7f14bd7f2f06071fcd2
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2017-03-23 20:18:54 +0200

    Add command for importing data from infopankki dump

--- info/management/commands/import.py
+++ info/management/commands/import.py
@@ -1,11 +1,18 @@
 from django.core.management.base import BaseCommand
+import info.importer
+from info.models import MasterPage, Page
 
 
 class Command(BaseCommand):
-    help = "My shiny new management command."
+    help = "Import infopankki site dump into database."
 
-    def add_arguments(self, parser):
-        parser.add_argument('sample', nargs='+')
+    # def add_arguments(self, parser):
+    #     parser.add_argument('sample', nargs='+')
 
     def handle(self, *args, **options):
-        raise NotImplementedError()
+        print('Start of import')
+        print('Before import master page count', MasterPage.objects.count())
+        print('Before import page count', Page.objects.count())
+        info.importer.do_import()
+        print('After import master page count', MasterPage.objects.count())
+        print('After import page count', Page.objects.count())
