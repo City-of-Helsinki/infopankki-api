619cda3f07244d49b6b8f194310d9e5fee7de60f
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2017-03-27 11:55:11 +0300

    More robust sync script

--- transfer/sync.sh
+++ transfer/sync.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-export INFOPANKKI_ROOT = $HOME/infopankki/
+export INFOPANKKI_TRANSFER_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd ${INFOPANKKI_ROOT}/api
+cd $INFOPANKKI_TRANSFER_ROOT/..
 
-lftp -f ${INFOPANKKI_ROOT}/infopankki_api/transfer/lftp_sync
+echo "Transfer infopankki data"
+lftp -f ${INFOPANKKI_TRANSFER_ROOT}/lftp_sync
 
-$HOME/info/venv/python ${INFOPANKKI_ROOT}/manage.py
+$HOME/info/venv/python ${INFOPANKKI_TRANSFER_ROOT}/../manage.py
 
+echo "Calling health check"
 curl --retry 3 https://hchk.io/c993b23c-ddc3-49b9-8169-7407d630d7f7
-
