86ce419e04a0f4eb9aa209d40376634f81761148
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2017-04-19 18:32:39 +0300

    Error handling for sync script

--- transfer/sync.sh
+++ transfer/sync.sh
@@ -7,8 +7,17 @@ cd $INFOPANKKI_TRANSFER_ROOT/..
 echo "Transfer infopankki data"
 lftp -f ${INFOPANKKI_TRANSFER_ROOT}/lftp_sync
 
+rc=$?;
+if [[ ${rc} != 0 ]]; then exit ${rc}; fi
+
 echo "Import startup"
 env $(cat .env | grep -v "^#" | xargs) $HOME/info/venv/bin/python manage.py import
 
+rc=$?;
+if [[ ${rc} != 0 ]]; then exit ${rc}; fi
+
 echo "Calling health check"
 curl --retry 3 https://hchk.io/c993b23c-ddc3-49b9-8169-7407d630d7f7
+
+rc=$?;
+if [[ ${rc} != 0 ]]; then exit ${rc}; fi
