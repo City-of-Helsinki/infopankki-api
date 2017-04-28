74a9a8094161ffd1232db9a0944d4ec7fe1c731a
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2017-04-28 19:22:04 +0300

    Two new views: one for master pages and one detail

--- info/urls.py
+++ info/urls.py
@@ -2,12 +2,13 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from .api import PageViewSet
-from .views import master_view
+from .views import master_view, page_view
 
 router = routers.DefaultRouter()
 router.register(r'pages', PageViewSet)
 
 urlpatterns = [
-    url(r'translations$', master_view),
+    url(r'translations/(.*)$', page_view, name='master_page'),
+    url(r'translations$', master_view, name='master_pages'),
     url(r'^', include(router.urls)),
 ]
--- info/views.py
+++ info/views.py
@@ -1,13 +1,25 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import MasterPage
+from .models import Page
+
+
+def page_view(request, page):
+    page = get_object_or_404(Page, pk=page)
+    siblings = [p for p in page.master.pages.all() if p.language != 'fi']
+    return render(request=request,
+                  template_name='info/translations_page.html',
+                  context={
+                      "page": page,
+                      "siblings": siblings
+                  })
 
 
 def master_view(request):
-    return render(request=request,
-                  template_name='info/translations.html',
-                  context={
-                      "master_pages": MasterPage.objects.filter(pages__isnull=False)
-                  })
 
+    pages = Page.objects.filter(language='fi')
+    return render(request=request,
+                  template_name='info/translations_master.html',
+                  context={
+                      "pages": pages
+                  })
