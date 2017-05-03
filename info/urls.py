ce00692dc903e3f35b80dc66f499de0edeae7ddd
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2017-05-03 23:46:53 +0300

    Upgrade DRF and enable documentation site

--- info/urls.py
+++ info/urls.py
@@ -1,5 +1,6 @@
 
 from django.conf.urls import url, include
+from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from .api import PageViewSet
 from .views import master_view, page_view, translate_view
@@ -11,5 +12,6 @@ urlpatterns = [
     url(r'translate/(.*)$', translate_view, name='translate_page'),
     url(r'translations/(.*)$', page_view, name='master_page'),
     url(r'translations$', master_view, name='master_pages'),
+    url(r'^docs/', include_docs_urls(title='Infopankki API')),
     url(r'^', include(router.urls)),
 ]
