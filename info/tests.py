ea732e800fa8aa2af5cd6485e9093a68734b2e15
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2019-07-09 13:30:42 +0300

    Add test to test azure pipeline

--- info/tests.py
+++ info/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from info.models import MasterPage
 
-# Create your tests here.
+
+class InfoTestCase(TestCase):
+    
+    def setUp(self):
+        MasterPage.objects.create(meta=dict(one="text", other="text"), page_guid="guid")
+    
+    def test_master_page(self):
+        obj = MasterPage.objects.get(page_guid="guid")
+        assert obj.meta['one'] == "text"
